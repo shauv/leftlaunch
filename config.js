e0f16258015e81e4e33d7453cb680f24fd113c74
shauv <shauv@users.noreply.github.com>
2025-09-16 23:17:39 +0100

    line indents fixed

--- config.js
+++ config.js
@@ -17,31 +17,31 @@ window.STARTPAGE_CONFIG = {
 	},
 	bookmarks: [
 		{
-			row: "1",
+		row: "1",
 			items: [
 				{ name: "steam", url: "https://steamcommunity.com/profiles/76561199767988119" },
-        { name: "steamdb", url: "https://steamdb.info/sales/?displayOnly=Game&min_discount=50&min_rating=90&min_reviews=2000&os=macos&sort=rating_desc" },
+        		{ name: "steamdb", url: "https://steamdb.info/sales/?displayOnly=Game&min_discount=50&min_rating=90&min_reviews=2000&os=macos&sort=rating_desc" },
 				{ name: "steamhunters", url: "https://steamhunters.com/id/shauv/games" },
 				{ name: "steamgifts", url: "https://www.steamgifts.com/" },
 				{ name: "pagywosg", url: "https://pagywosg.xyz/events" }
 			]
 		},
 		{
-			row: "2",
+		row: "2",
 			items: [
-        { name: "animekai", url: "https://animekai.to/" },
-        { name: "myanimelist", url: "https://myanimelist.net/profile/shauv" },
+        		{ name: "animekai", url: "https://animekai.to/" },
+        		{ name: "myanimelist", url: "https://myanimelist.net/profile/shauv" },
 				{ name: "reddit", url: "https://www.reddit.com/" },
-        { name: "youtube", url: "https://www.youtube.com/feed/subscriptions" },
+        		{ name: "youtube", url: "https://www.youtube.com/feed/subscriptions" },
 				{ name: "github", url: "https://github.com/shauv/leftlaunch" }
 			]
 		},
 		{
-			row: "3",
+		row: "3",
 			items: [
 				{ name: "monkeytype", url: "https://monkeytype.com/" },
-        { name: "office", url: "https://www.office.com/" },
-        { name: "copilot", url: "https://copilot.microsoft.com/" },
+        		{ name: "office", url: "https://www.office.com/" },
+        		{ name: "copilot", url: "https://copilot.microsoft.com/" },
 				{ name: "vanguard", url: "https://www.vanguardinvestor.co.uk/" },
 				{ name: "translate", url: "https://translate.google.com/" }
 			]
